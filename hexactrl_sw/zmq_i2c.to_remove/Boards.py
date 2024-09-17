fb03fffa7a01ecea439ee23d91428d94e36b40ba
Kamisato rize <72055275+Anurag-Sr@users.noreply.github.com>
2024-09-17 11:08:57 +0200

    Add files via upload

---
+++ hexactrl_sw/zmq_i2c.to_remove/Boards.py
@@ -0,0 +1,230 @@
+from ROC import ROC
+from Translator import Translator
+from nested_dict import nested_dict
+from nested_lookup import get_all_keys, nested_update
+from threading import Thread
+from itertools import groupby
+
+import glob
+
+class PropagatingThread(Thread):
+    """ Thread class to propagate occuring Exceptions to the calling thread. """
+
+    def run(self):
+        self.exc = None
+        try: self.ret = self._target(*self._args, **self._kwargs)
+        except BaseException as e: self.exc = e     # store exception
+
+    def join(self):
+        super(PropagatingThread, self).join()
+        if self.exc: raise self.exc
+        return self.ret
+import yaml
+class CharBoard():
+    """ Base class for characterization boards """
+
+    def __init__(self, links):
+        self.rocs = {name:ROC(link) for (name, link) in links.items()}
+        self.writeCaches = {name:{} for name in links.keys()}  # dicts are thread-safe, so we can write to it from different threads
+        for rname, roc in self.rocs.items(): 
+            roc.reset()
+            print('[%s] GPIO reset' % rname)
+        roc_type = self.__detect_roc_type()         # Determine which ROC architecture we're on.
+        self.translator = Translator(roc_type)      
+
+    def configure(self, cfgs):
+
+        for lbl, cfg in cfgs.items():
+            for rname in [name for name in self.rocs.keys() if lbl in name]:
+                roc = self.rocs[rname]
+                try:
+                    self.__write(roc,rname,cfg)
+                except Exception as e:                  # catch i2c exceptions.
+                    print("ERROR in configure: ", e)
+                    for rname, roc in self.rocs.items(): 
+                        print('[%s] GPIO reset' % rname)
+                        roc.reset()
+                    for lbl, roc in self.rocs.items(): 
+                        sortedPairs = self.translator.sort_pairs(self.writeCaches[lbl])
+                        roc.write(sortedPairs)                                                      # Rewrite caches
+                    return self.configure(cfgs)                                                     # Reload cfgs
+
+        return "ROC(s) CONFIGURED"
+
+    def read(self, cfgs=""):
+        """ Factory method for unified interface to write. """
+
+        if cfgs: return self.__read_fr_cfgs(cfgs)
+        else: return self.__read_fr_cache()
+
+    def reset_tdc(self):
+        """ Reset MasterTDC parameter for all ROCs. """
+
+        self.configure({lbl:{"MasterTdc":{"all":{"START_COUNTER":0}}} for lbl in self.rocs.keys()})
+        self.configure({lbl:{"MasterTdc":{"all":{"START_COUNTER":1}}} for lbl in self.rocs.keys()})
+        return "masterTDCs reset."
+
+    def __write(self, roc, roc_name, cfg):
+        """ Stuff that should run in a separate thread per ROC. """
+        pairs = self.translator.pairs_from_cfg(cfg, self.writeCaches[roc_name], roc)
+        self.writeCaches[roc_name].update(pairs)
+        sortedPairs = self.translator.sort_pairs(pairs)
+        roc.write(sortedPairs)
+        print('[%s] Configured' % roc_name)
+
+    def __read_fr_cache(self):
+        """ Read addresses in write_param cache from rocs. """
+
+        rd_cfgs = {}
+        for lbl, roc in self.rocs.items():
+            pairs = self.writeCaches[lbl]
+            sortedPairs = self.translator.sort_pairs(pairs)
+            rd_pairs = roc.read(sortedPairs)
+            rd_cfg = self.translator.cfg_from_pairs(rd_pairs)
+            rd_cfgs[lbl] = rd_cfg
+        return rd_cfgs
+
+    def __read_fr_cfgs(self, cfgs):
+        """ Read addresses (=keys) in cfgs from rocs. """
+
+        rd_cfgs = {}
+        for lbl, cfg in cfgs.items():
+            for roc_name in [name for name in self.rocs.keys() if lbl in name]:
+                roc = self.rocs[roc_name]
+                req_keys = set(key[-1] for key in nested_dict(cfg).keys_flat())
+                pairs = self.translator.pairs_from_cfg(cfg, self.writeCaches[roc_name],roc)
+                sortedPairs = self.translator.sort_pairs(pairs)
+                rd_pairs = roc.read(sortedPairs)
+                rd_cfg = self.translator.cfg_from_pairs(rd_pairs)	# params in same reg are also read..
+                req_cfg = nested_dict()		                        # .. so only return requested config.
+                for idx, val in nested_dict(rd_cfg).items_flat():
+                    if idx[-1] in req_keys: 	                    # idx=(block,blockID,param)
+                        req_cfg[idx[0]][idx[1]][idx[2]] = val
+                rd_cfgs[roc_name] = req_cfg.to_dict()
+        return rd_cfgs
+
+    def __detect_roc_type(self):
+        """ Query ROCs to detect if we're on Si or SiPM. """
+
+        for roc in self.rocs.values():
+            Glob_Ana_reg0_half0 = (32, 37)
+            probe_pair = [[{Glob_Ana_reg0_half0: None}]]
+            answer = roc.read(probe_pair)
+            val = answer[Glob_Ana_reg0_half0]
+            
+        #====================test
+        print("answer:")
+        print(answer)
+        print("ROC value from register 32, 37:")
+        print(val)
+        #===================
+    
+        if val == 130: 
+            print('[roc_s*] Detected Si type')
+            return "Si"
+        #elif val == 143: 
+        elif val == 79:
+            print('roc_s*] Detected SiPM type')
+            return "SiPM"
+
+        #================================add by Jia-Hao 14022023
+        elif val == 0:
+            print('[roc_s*] Detected SiPMv3 type')
+            return "SiPMv3"
+        #===============================================        
+
+        else:
+            return "Siv3"
+    
+class HexaBoard(CharBoard):
+    """
+    HexaBoard differs from CharBoard by:
+    1. Board Orientation (alignment of Trophy i2c labels with ROC addresses found on bus)
+    2. On-board Trophy ADCs
+    """
+    def __init__(self, links):
+        super().__init__(links)
+
+        self.adc_pwr = {}
+        self.adc_probeDC = {}
+        self.adc_cal = {}
+        i2c_labels = glob.glob("/sys/class/i2c-dev/i2c-*/device/*/of_node/label")
+        for labelfile in i2c_labels:
+            with open(labelfile) as fin:
+                label = fin.readline()
+                if label.find('adc_pwr')==0:
+                    self.adc_pwr['piv_a']=labelfile.split('of_node')[0]+'in4_input'
+                    self.adc_pwr['piv_d']=labelfile.split('of_node')[0]+'in5_input'
+                elif label.find('adc_s0')==0:
+                    self.adc_probeDC['roc_s0']=labelfile.split('of_node')[0]+'in5_input'
+                    self.adc_cal['roc_s0']    =labelfile.split('of_node')[0]+'in4_input'
+                elif label.find('adc_s1')==0:
+                    self.adc_probeDC['roc_s1']=labelfile.split('of_node')[0]+'in5_input'
+                    self.adc_cal['roc_s1']    =labelfile.split('of_node')[0]+'in4_input'
+                elif label.find('adc_s2')==0:
+                    self.adc_probeDC['roc_s2']=labelfile.split('of_node')[0]+'in5_input'
+                    self.adc_cal['roc_s2']    =labelfile.split('of_node')[0]+'in4_input'
+        
+    def read_pwr(self):
+        """ Return reading of TrophyBoard's analog (piv_a) and digital (piv_d) ADCs. """
+
+        res = {}
+        for lbl, cmd in self.adc_pwr.items():
+            with open(cmd) as f:
+                res[lbl] = f.read().rstrip('\n')
+        return res
+
+    def read_adc(self, cfgs):
+        """
+        Return reading of TrophyBoard's probeDC and InCtest ADCs.
+        Since both halves of a chip and all the chips in a sector are wired together for one ADC,
+        we have to set and read probeDC and calibDAC sequentially per chip and per half.
+        Orientation of HexaBoard determines which ADC (in TrophyBoard sectors) have to be read.
+        """
+
+        pdc_adc = {'roc_s0': '/sys/class/i2c-dev/i2c-2/device/2-0048/in5_input',
+                   'roc_s1': '/sys/class/i2c-dev/i2c-3/device/3-0049/in5_input',
+                   'roc_s2': '/sys/class/i2c-dev/i2c-1/device/1-0048/in5_input', }
+
+        cal_adc = {'roc_s0': '/sys/class/i2c-dev/i2c-2/device/2-0048/in4_input',
+                   'roc_s1': '/sys/class/i2c-dev/i2c-3/device/3-0049/in4_input',
+                   'roc_s2': '/sys/class/i2c-dev/i2c-1/device/1-0048/in4_input', }
+
+        cfg_keys = get_all_keys(cfgs)    # scan the received cfg for 'Calib_dac' or 'Probe_dcX'
+        if 'Calib' in cfg_keys: 
+            adc  = self.adc_cal#cal_adc
+            keys = ['IntCtest', 'ExtCtest', 'Calib']
+        elif any('probe_dc' in str(key) for key in cfg_keys): 
+            adc  = self.adc_probeDC#pdc_adc
+            keys = [key for key in cfg_keys if 'probe_dc' in key]
+
+        # expand original cfgs (ocfgs) and zero'd cfgs (zcfgs)
+        res = nested_dict()
+        print(yaml.dump(cfgs))
+        ocfgs = self.translator.expand_cfgs(cfgs, self.rocs)
+        zcfgs = ocfgs.copy()
+        for key in keys: zcfgs = nested_update(zcfgs, key=key, value=0)
+        
+        # group ROCs that can be configured in parallel
+        rocs = sorted(list(ocfgs.keys()), key=lambda roc: roc[-1])
+        confGroup = [list(g) for _, g in groupby(rocs, key=lambda roc: roc[-1])]
+
+        for sel_half in [0,1]:
+            for group in confGroup:
+                # create cfgs for ROCs and Halves that can be configured together
+                ncfgs = nested_dict(zcfgs)
+                for roc in group:
+                    ncfgs[roc]['ReferenceVoltage'][sel_half] = ocfgs[roc]['ReferenceVoltage'][sel_half]
+
+                # configure, readout & save
+                self.configure(ncfgs.to_dict())
+                for roc in group:
+                    try:
+                        with open(adc[roc]) as f:
+                            res[roc][sel_half] = f.read().rstrip('\n')
+                    except:
+                        res[roc][sel_half] = 0
+                        continue
+
+        self.configure(zcfgs)   # deconfigure
+        return res.to_dict()
---
+++ hexactrl_sw/zmq_i2c.to_remove/Link_original.py
@@ -0,0 +1,272 @@
+from smbus2 import SMBus
+import gpiod
+
+class LinkBuilder:
+    def __init__(self):
+
+       i2cs = {}
+       gpios = {}
+  
+       try:
+           i2cs.update(xil_i2c_discover())
+           gpios.update(xil_gpio_discover())
+       except (IOError):
+           pass
+
+       try:
+          import gbtsca_bus
+          try:
+             self.sca = gbtsca_bus.GBTSCA("gbt_sca_com_0","file://${UHAL_ADDRESS_TABLE}/connection.xml", "mylittlememory")
+             self.sca.reset_gbtsca() 
+          except(gbtsca_bus.sca.gbtsca_exception.GBT_SCA_Exception):
+             pass
+          
+          try:
+              gpios.update(sca_gpio_discover(self.sca))
+              for gpio_name, gpio in gpios.items():
+                 gpio.write(1)
+              i2cs.update(sca_i2c_discover(self.sca))
+          except(gbtsca_bus.sca.gbtsca_exception.GBT_SCA_Exception):
+              pass
+
+       except(ImportError):
+          pass
+
+       self.links = {}
+       for gpio_name, gpio in gpios.items():
+           for i2c_name, i2c in i2cs.items():
+               if gpio_name in i2c_name:
+                   self.links[i2c_name] = Link(i2c, gpio)
+
+class Link:
+    def __init__(self, i2c, gpio):
+        self.i2c = i2c
+        self.gpio = gpio
+
+class i2c():
+    def __init__(self, addr, bus, sca=0):
+        self._addr = addr
+        self._bus = bus
+        self._sca = sca
+
+    def write(self, *args, **kwargs):
+        raise NotImplementedError
+
+    def read(self, *args, **kwargs):
+        raise NotImplementedError
+
+class sca_i2c(i2c):
+    def write(self, offset, byte):
+       with self._sca.scabus(self._bus) as bus:
+            bus.write_byte(self._addr + offset, byte)
+       return None
+    
+    def read(self, offset):
+       with self._sca.scabus(self._bus) as bus:
+            ret = bus.read_byte(self._addr + offset)
+       return ret
+
+def sca_i2c_discover(sca):
+    """ Discover all ROCs on gbtsca i2c """
+
+    rocs = []
+    sca.enableI2C(0xffff)
+    for bus_id in range(15):  # 8 i2c bus lines
+        try:
+            with sca.scabus(bus_id) as bus:
+                addrs = []
+                for addr in range(128):  # 128 addrs per bus line
+                    try:
+                        bus.read_byte(addr)
+                        addrs.append(addr)
+                    except Exception as e:
+                        pass # skip non-existing addr
+                print('[I2C] Found %d address(es) on bus %d' % (len(addrs),bus_id))
+                if len(addrs) >= 8: 
+                    rocs.append((addrs[0], bus_id))
+                if len(addrs) == 16:
+                    rocs.append((addrs[8], bus_id))
+        except Exception as e:
+            pass  # skip undefined i2c busses
+    return sca_i2c_create(rocs,sca)
+
+def sca_i2c_create(rocs,sca):
+    """ Detect board type & create i2c objects """
+
+    n = len(rocs)
+    if n == 1:   
+        print('[I2C] Identified Single-Chip (Char) board')
+        roc_map = i2c_char_map
+    elif n == 3: 
+        print('[I2C] Identified LD HexaBoard')
+        roc_map = i2c_ld_map
+    elif n == 6: 
+        print('[I2C] Identified HD HexaBoard')
+        roc_map = i2c_hd_map
+    return {roc_map[addr]:sca_i2c(addr,bus,sca) for (addr, bus) in rocs}
+
+class xil_i2c(i2c):
+    def write(self, offset, byte):
+        with SMBus(self._bus) as bus:
+            bus.write_byte(self._addr + offset, byte)
+        return None
+
+    def read(self, offset):
+        with SMBus(self._bus) as bus:
+            ret = bus.read_byte(self._addr + offset)
+        return ret
+
+i2c_char_map = {0x28: 'roc_s0'}
+i2c_ld_map_v1 = {0x00: 'roc_s0', 0x40: 'roc_s1', 0x20: 'roc_s2'} #old LD boards for ROCv2
+i2c_ld_map_v2 = {0x60: 'roc_s0', 0x40: 'roc_s1', 0x20: 'roc_s2'} #nsh boards
+i2c_ld_map_v3 = {0x08: 'roc_s0', 0x18: 'roc_s1', 0x28: 'roc_s2'} #LD V3 boards
+
+# i2c_hd_map = {0x18: 'roc_s0_0', 0x30: 'roc_s0_1', 0x08: 'roc_s1_0', 0x20: 'roc_s1_1', 0x10: 'roc_s2_0', 0x28: 'roc_s2_1'}
+i2c_hd_map = {0x8: 'roc_s0_0', 0x48: 'roc_s0_1', 0x18: 'roc_s1_0', 0x58: 'roc_s1_1', 0x28: 'roc_s2_0', 0x68: 'roc_s2_1'}
+
+def xil_i2c_discover():
+    """ Discover all ROCs on i2c """
+
+    rocs = []
+    for bus_id in range(8):  # 8 i2c bus lines
+        try:
+            with SMBus(bus_id) as bus:
+                addrs = []
+                for addr in range(128):  # 128 addrs per bus line
+                    try:
+                        bus.read_byte(addr)
+                        addrs.append(addr)
+                    except IOError as e: 
+                        pass # skip non-existing addr
+                print('[I2C] Found %d address(es) on bus %d' % (len(addrs),bus_id))
+                if len(addrs) >= 8: 
+                    rocs.append((addrs[0], bus_id))
+                if len(addrs) >= 16:
+                    rocs.append((addrs[8], bus_id))
+                if len(addrs) >= 24:
+                    rocs.append((addrs[16], bus_id))
+        except FileNotFoundError:
+            pass  # skip undefined i2c busses
+    return xil_i2c_create(rocs)
+
+def xil_i2c_create(rocs):
+    """ Detect board type & create i2c objects """
+
+    n = len(rocs)
+    print(rocs)
+    if n == 1:   
+        print('[I2C] Identified Single-Chip (Char) board')
+        roc_map = i2c_char_map
+    elif n == 3: 
+        print('[I2C] Identified LD HexaBoard')
+        if rocs[0][0] in i2c_ld_map_v1:
+            i2c_map = i2c_ld_map_v1
+        elif rocs[0][0] in i2c_ld_map_v2:
+            i2c_map = i2c_ld_map_v2
+        elif rocs[0][0] in i2c_ld_map_v3:
+            i2c_map = i2c_ld_map_v3
+        else:
+            print("ERROR : fail to find correct I2C map in xil_i2c_create")
+            i2c_map = {}
+        roc_map = i2c_map
+    elif n == 6: 
+        print('[I2C] Identified HD HexaBoard')
+        roc_map = i2c_hd_map
+    return {roc_map[addr]:xil_i2c(addr,bus) for (addr, bus) in rocs}
+
+class gpio():
+    def __init__(self, lines):
+        self._lines = lines
+
+    def write(self, *args, **kwargs):
+        raise NotImplementedError
+    
+    def read(self, *args, **kwargs):
+        raise NotImplementedError
+
+gpio_tbtester_map = {'roc_s0':[('hgcroc_soft_rstB',0x4), ('hgcroc_i2c_rstB',0x8), ('hgcroc_hard_rstB',0x10)]}
+gpio_tbtester_noresetmap = {'roc_s0':[('EN_LDO',0x400000), ('SOFTSTART',0x800000)]}
+
+class sca_gpio(gpio):
+    def write(self, val):
+       for line in self._lines:
+            line.set_value(val)
+  
+    def read(self):
+        ans = { }
+        for line in self._lines:
+            ans[line.name] = line.get_value()
+        return ans
+ 
+    def reset(self):
+        self._lines[0].reset_rocs()
+
+def sca_gpio_discover(sca):
+    lines = []
+    sca.enableGPIO(True)
+    for name in gpio_tbtester_map['roc_s0']:
+        lines.append(sca.gpio(name[0],name[1],False));
+    for name in gpio_tbtester_noresetmap['roc_s0']:
+        line = sca.gpio(name[0],name[1],False)
+        line.set_value(1)
+    return {'roc_s0' : sca_gpio(lines)}   
+
+class xil_gpio(gpio):
+
+    def write(self, val):
+        for line in self._lines:
+            config = gpiod.line_request()
+            config.consumer = "xil_gpio_write"
+            config.request_type = gpiod.line_request.DIRECTION_OUTPUT
+            line.request(config)
+            line.set_value(val)
+            line.release()
+
+    def read(self):
+        ans = {}
+        for line in self._lines:
+            config = gpiod.line_request()
+            config.consumer = "xil_gpio_read"
+            config.request_type = gpiod.line_request.DIRECTION_INPUT
+            line.request(config)
+            ans[line.name] = line.get_value()
+        return ans
+
+gpio_char_map = {'roc_s0': ['hgcroc_rstB', 'resyncload', 'hgcroc_i2c_rstB']}
+
+## for trophy v1 and v2
+gpio_hexa_map_v1 = {'roc_s0': ['s0_resetn', 's0_i2c_rstn'], 
+                    'roc_s1': ['s1_resetn', 's1_i2c_rstn'], 
+                    'roc_s2': ['s2_resetn', 's2_i2c_rstn']}
+
+## for trophy v3
+gpio_hexa_map_v3 = {'roc_s0': ['s1_resetn', 'hard_resetn'], 
+                    'roc_s1': ['s2_resetn', 'hard_resetn'], 
+                    'roc_s2': ['s3_resetn', 'hard_resetn']}
+
+def xil_gpio_discover():
+    # get all lines on all connected gpio chips
+    lines = []
+    for chip in gpiod.chip_iter():
+        for line in gpiod.line_iter(chip):
+            lines.append(line)
+    print([l.name for l in lines],'hgcroc_rstB' in [l.name for l in lines])        
+    if 'hgcroc_rstB' in [l.name for l in lines]:
+        gpio_map = gpio_char_map
+    elif 's0_i2c_rstn' in [l.name for l in lines]:
+        gpio_map = gpio_hexa_map_v1
+    elif 'hard_resetn' in [l.name for l in lines]:
+        gpio_map = gpio_hexa_map_v3
+    else:
+        print("ERROR : fail to find correct GPIO map in xil_gpio_discover")
+        gpio_map = {}
+
+    print("[GPIO] Identified gpio map : ",gpio_map)
+    ret = {}
+    print(gpio_map)
+    for roc, line_names in gpio_map.items():
+        sel_names = set(line_names).intersection([l.name for l in lines])
+        if sel_names:
+            ret[roc] = xil_gpio([l for l in lines if l.name in sel_names])
+    if ret: return ret
+    else: raise('Missing HexaBoard GPIO lines.')
